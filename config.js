c3afb0a4c1a0e5b3da14727e5963dc4dc6fe7add
FelipeODEUS <92820937+FelipeODEUS@users.noreply.github.com>
2022-06-19 18:39:15 -0300

    votação no trabalho

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyBH8NfUyQNOPAnowsgOtR2cI9t8GdAt_U0",
+    authDomain: "vote-9b7f5.firebaseapp.com",
+    databaseURL: "https://vote-9b7f5-default-rtdb.firebaseio.com",
+    projectId: "vote-9b7f5",
+    storageBucket: "vote-9b7f5.appspot.com",
+    messagingSenderId: "28251336981",
+    appId: "1:28251336981:web:4231c3227dbc97a64c391c"
+  };
 
-};
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
 
