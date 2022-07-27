3bee526d93832e06f02f8610aadfd689218e01a7
John-Voss <joao.vitor_voss@hotmail.com>
2022-07-27 20:58:35 -0300

    projeto67

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+    apiKey: "AIzaSyC-86m_ulfMsi0HMpx-xkCZq8EXVAMKukU",
+    authDomain: "projeto-67-83de3.firebaseapp.com",
+    databaseURL: "https://projeto-67-83de3-default-rtdb.firebaseio.com",
+    projectId: "projeto-67-83de3",
+    storageBucket: "projeto-67-83de3.appspot.com",
+    messagingSenderId: "203438974490",
+    appId: "1:203438974490:web:c960d6e75a73bbb49913a3"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
