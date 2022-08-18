9e4a3eb53e01af846c3f7ef6ea694cdef172cf19
natis2 <nati0151919@gmail.com>
2022-08-18 16:57:00 -0300

    projeto da Nati

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
-
-};
+    const firebaseConfig = {
+        apiKey: "AIzaSyBdkXIIwXximic81iggy3PjxY-n-z7aKik",
+        authDomain: "projeto-67-6cecf.firebaseapp.com",
+        databaseURL: "https://projeto-67-6cecf-default-rtdb.firebaseio.com",
+        projectId: "projeto-67-6cecf",
+        storageBucket: "projeto-67-6cecf.appspot.com",
+        messagingSenderId: "609396474902",
+        appId: "1:609396474902:web:1063a56dd10a0668b0e137"
+      };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
 
