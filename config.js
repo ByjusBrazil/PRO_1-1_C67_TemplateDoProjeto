89f2a620f88d4a6d143c8b41b0a0a378136d7c4a
manubueno13 <manuh.bueno2006@gmail.com>
2022-09-21 16:54:55 -0300

    mensagem

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyAEMv8y-03xXAeEhPN-ndsu0pmEZSa3X0A",
+    authDomain: "projeto-58-f4c86.firebaseapp.com",
+    databaseURL: "https://projeto-58-f4c86-default-rtdb.firebaseio.com",
+    projectId: "projeto-58-f4c86",
+    storageBucket: "projeto-58-f4c86.appspot.com",
+    messagingSenderId: "464507060644",
+    appId: "1:464507060644:web:6f0952079b7a87a5e94131"
+  };
 
-};
+
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
 
