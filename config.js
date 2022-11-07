9cff624afb6d9f38867bc3850e4190e81216bb99
breadcheroso <giancarlorsanti@gmail.com>
2022-11-07 17:13:35 -0300

    mudei a cor e adicionei um bd

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAZasSCxH4fUoaeBkTM9moDoVysMkIQdHk",
+    authDomain: "proj58-ecf21.firebaseapp.com",
+    databaseURL: "https://proj58-ecf21-default-rtdb.firebaseio.com",
+    projectId: "proj58-ecf21",
+    storageBucket: "proj58-ecf21.appspot.com",
+    messagingSenderId: "731373645534",
+    appId: "1:731373645534:web:58a1a099da13e34b2f1c4f",
+    measurementId: "G-H7L6V96JC2"
+  };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
 
