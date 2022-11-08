6406b48d624d2da3f01f11922efd7ea030df25b8
pedrobenkoYT <89105930+pedrobenkoYT@users.noreply.github.com>
2022-11-08 14:42:49 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
+    const firebaseConfig = {
+        apiKey: "AIzaSyDfsGDNj-JOgsTT_URSEx7Bg8BaRYD-rcw",
+        authDomain: "codigo-aberto-do-peixe-boi.firebaseapp.com",
+        databaseURL: "https://codigo-aberto-do-peixe-boi-default-rtdb.firebaseio.com",
+        projectId: "codigo-aberto-do-peixe-boi",
+        storageBucket: "codigo-aberto-do-peixe-boi.appspot.com",
+        messagingSenderId: "735067144214",
+        appId: "1:735067144214:web:5e04ba6a9df707e1553537"
+      };
 
-};
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
+
 
 export default firebase.database();
