f71c8549c79c77b90061cb73fed66d6d12be6757
unknown <edgarlodulassis@gmail.com>
2022-12-01 18:43:07 -0300

    modificaçao do botao

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
+const firebaseConfig = { apiKey: "AIzaSyBVljdzKErSjY3SPFlCoGbaNqz0e031PZA", authDomain: "projeto-58-a072b.firebaseapp.com", databaseURL: "https://projeto-58-a072b-default-rtdb.firebaseio.com", projectId: "projeto-58-a072b", storageBucket: "projeto-58-a072b.appspot.com", messagingSenderId: "1059567849038", appId: "1:1059567849038:web:41b9a2890ba51348367d1f" };
 
-};
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
 
