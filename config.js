1dded2642c44f97e147557395a7653cb97170734
Mossmann34 <99044412+Mossmann34@users.noreply.github.com>
2023-03-09 17:08:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,13 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
+  
+  apiKey: "AIzaSyAAPX_9-vkZY2l4qyChQjoCSrjAYNWK6wM",
+  authDomain: "aulateste-97d65.firebaseapp.com",
+  projectId: "aulateste-97d65",
+  storageBucket: "aulateste-97d65.appspot.com",
+  messagingSenderId: "1067682544683",
+  appId: "1:1067682544683:web:fe3716434ddd5fe8fd3e6e"
 
 };
 // Initialice o Firebase
