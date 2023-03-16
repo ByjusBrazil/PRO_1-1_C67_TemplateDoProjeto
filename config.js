5470614207009a048533c3ce981dd3121d7db36e
brunohacker243 <99298982+brunohacker243@users.noreply.github.com>
2023-03-16 20:33:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAr6--dfc8daUGLq9Ttrq1xCzR-2tBXDi0",
+  authDomain: "byjus-project-67.firebaseapp.com",
+  projectId: "byjus-project-67",
+  storageBucket: "byjus-project-67.appspot.com",
+  messagingSenderId: "1014316907384",
+  appId: "1:1014316907384:web:73f2832f03b5b39987668d"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
