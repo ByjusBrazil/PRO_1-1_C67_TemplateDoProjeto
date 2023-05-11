03146e5734cd18acc076baee0107563e09966390
InfamousPlayer <felipemontenegro2003@gmail.com>
2023-05-11 10:06:53 -0300

    SDK

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+  apiKey: "AIzaSyBiQAnSU3VdXXyUgV9p07ZukTx2GZAJowM",
+  authDomain: "teamvotingapp-9eade.firebaseapp.com",
+  databaseURL: "https://teamvotingapp-9eade-default-rtdb.firebaseio.com",
+  projectId: "teamvotingapp-9eade",
+  storageBucket: "teamvotingapp-9eade.appspot.com",
+  messagingSenderId: "577889306954",
+  appId: "1:577889306954:web:044131df53059cf7bc99b3",
+  measurementId: "G-FM3R9J1RSY"
 
 };
 // Initialice o Firebase
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
