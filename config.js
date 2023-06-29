b7324b6e7ceccac6ba04d2f86b0c376d3f5b3f38
yaya2202 <100168103+yaya2202@users.noreply.github.com>
2023-06-29 17:29:51 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyDC__VSypinBF9lBle7sYzBaZG6_MYrVZQ",
+  authDomain: "projeto67-342b0.firebaseapp.com",
+  projectId: "projeto67-342b0",
+  storageBucket: "projeto67-342b0.appspot.com",
+  messagingSenderId: "102452380856",
+  appId: "1:102452380856:web:ae92a62943efa3c84fa04d",
+  measurementId: "G-M1F0JSBZVV"
+};
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
