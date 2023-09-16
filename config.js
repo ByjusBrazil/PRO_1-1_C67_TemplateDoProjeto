57f4b40484e9a1168ad6d155a95141629c12143b
arthur <arthurbatgub.com>
2023-09-16 16:50:51 -0300

    miau

--- config.js
+++ config.js
@@ -1,10 +1,20 @@
-import firebase from 'firebase';
+import firebase from 'firebase'
 
-// adicione SDK do Firebase
-var firebaseConfig = {
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBTt4LfvsAkzR1Gu2Mmf2epwe6LLeLNPu8",
+  authDomain: "app60-6ec39.firebaseapp.com",
+  databaseURL: "https://app60-6ec39-default-rtdb.firebaseio.com",
+  projectId: "app60-6ec39",
+  storageBucket: "app60-6ec39.appspot.com",
+  messagingSenderId: "738417520874",
+  appId: "1:738417520874:web:0d99ef5673b61015b82793"
 };
-// Initialice o Firebase
+
+const app = initializeApp(firebaseConfig);
+
+
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
