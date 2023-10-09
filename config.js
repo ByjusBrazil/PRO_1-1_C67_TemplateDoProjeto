b6c7d0507424ca0212df167703cffc1dcc17f71a
BeatrizYukiAsano <113141229+BeatrizYukiAsano@users.noreply.github.com>
2023-10-09 17:20:52 -0300

    Inserção da chave do banco de dados

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 import firebase from 'firebase';
 
-// adicione SDK do Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCPQGs2D3lDniRvGDnYb2-8qZJtYm8wd4A",
+  authDomain: "c-67-271f6.firebaseapp.com",
+  projectId: "c-67-271f6",
+  storageBucket: "c-67-271f6.appspot.com",
+  messagingSenderId: "1073202079453",
+  appId: "1:1073202079453:web:c58f327e062680e3499d34"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
