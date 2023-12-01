828188884e3854aa202e95eea1695a54bd2f5b78
Sarah Aimeé <lorencineaimee@gmail.com>
2023-12-01 17:15:50 -0300

    adicionei o firebase

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyCyfTlBrk9ra84UbndSwUR7bQuF-dfQytU",
+    authDomain: "pro-67-d57cd.firebaseapp.com",
+    databaseURL: "https://pro-67-d57cd-default-rtdb.firebaseio.com",
+    projectId: "pro-67-d57cd",
+    storageBucket: "pro-67-d57cd.appspot.com",
+    messagingSenderId: "175811672507",
+    appId: "1:175811672507:web:e308cac1e2ccb33db16c9e"
+  };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
 
